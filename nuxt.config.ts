7e3fc20a4d57ffefca566ce3694185ab8d541506
Guillaume Claverie <guillaume351.claverie@gmail.com>
2024-08-08 09:36:10 +0200

    feat: favicon, title & meta description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"]
-})
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  app: {
+    head: {
+      title: "Cookie Build : Minecraft Mini-Games Server",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "The Classic Minecraft Mini-Games Server, for Java & Bedrock Editions 🎮 Pitchout and MicroBattles available now!",
+        },
+      ],
+    },
+  },
+});
